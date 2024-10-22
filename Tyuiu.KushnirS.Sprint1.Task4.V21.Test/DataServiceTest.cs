c354637dc840f8916e58420d94d546dc41bab96b
User <User@DESKTOP-8Q26HPT>
2024-10-22 20:45:49 +0500

    Task4

---
+++ Tyuiu.KushnirS.Sprint1.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.KushnirS.Sprint1.Task4.V21.Lib;
+namespace Tyuiu.KushnirS.Sprint1.Task4.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint1.Task4.V21/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KushnirS.Sprint1.Task4.V21.Lib;
+namespace Tyuiu.KushnirS.Sprint1.Task4.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            double x, y;
+            Console.WriteLine("Введите переменную x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите переменную y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadLine();
+        }
+    }
+}
