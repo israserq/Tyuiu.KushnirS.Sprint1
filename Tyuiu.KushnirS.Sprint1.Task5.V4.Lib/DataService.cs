619fc20216ca8364b4b83b0b5a599d1c9c306912
User <User@DESKTOP-8Q26HPT>
2024-10-22 20:55:50 +0500

    Task5

---
+++ Tyuiu.KushnirS.Sprint1.Task5.V4.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KushnirS.Sprint1.Task5.V4.Lib
+{
+    public class DataService : ISprint1Task5V4
+    {
+        public int SecondsToHours(int time)
+        {
+            var res = time / 60 / 60;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.KushnirS.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.KushnirS.Sprint1.Task5.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int time = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ds.SecondsToHours(time));
+        }
+    }
+}
